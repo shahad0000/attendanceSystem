34061ecb9710d092d319519829e508615a7f6793
user1 <user01@em/com>
2025-06-25 18:52:18 +0300

    attendance done

--- src/controllers/class.controller.ts
+++ src/controllers/class.controller.ts
@@ -1,14 +1,21 @@
-import { Request, Response, NextFunction } from "express";
-import { AuthRequest } from "../middleware/auth.middleware";
+import { Request, Response, NextFunction } from "express"; // Types from Express for typing request handlers
+import { AuthRequest } from "../middleware/auth.middleware"; // custom request type that includes the authenticated user (req.user)
+
+// Service functions for retrieving classes and creating a new class
 import { getClassesService } from "../services/class.services";
 import { createClassService } from "../services/admin.service";
+
+// Functions that handle logic for assigning students, teachers, and principals to a class
 import { assignStudentsService, assignTeachersService, assignPrincipalService } from "../services/class.services";
 
 export const createClass = async (req: AuthRequest, res: Response) => {
+
+  // only admin can create a class
   if (req.user?.role !== "admin") {
     res.status(403).json({ message: "Forbidden" });
     return;
   }
+
   try {
     const classData = req.body;
 
@@ -25,6 +32,9 @@ export const getClasses = async (req: Request, res: Response) => {
   res.json({ status: "success", data: classes });
 };
 
+// Extracts the class ID from the URL (/class/:id/students) and the list of studentIds from the body
+// Calls the service to update the class with the students
+// Sends the updated class
 export const assignStudentsToClass = async (req: AuthRequest, res: Response, next: NextFunction) => {
   try {
     const classId = req.params.id;
@@ -36,6 +46,7 @@ export const assignStudentsToClass = async (req: AuthRequest, res: Response, nex
   }
 };
 
+// Similar to the previous function but for assigning teachers to a class
 export const assignTeachersToClass = async (req: AuthRequest, res: Response, next: NextFunction) => {
   try {
     const classId = req.params.id;
@@ -47,6 +58,7 @@ export const assignTeachersToClass = async (req: AuthRequest, res: Response, nex
   }
 };
 
+// Assigns or replaces the principal of a class using the principalId provided in the body
 export const assignPrincipalToClass = async (req: AuthRequest, res: Response, next: NextFunction) => {
   try {
     const classId = req.params.id;
