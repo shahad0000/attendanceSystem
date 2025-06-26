502de623118fc4eb8c2249b59c1736635cd66d0e
user1 <user01@em/com>
2025-06-26 13:26:25 +0300

    update student report

--- src/controllers/student.controller.ts
+++ src/controllers/student.controller.ts
@@ -6,11 +6,7 @@ export const getReport = async (req: Request, res: Response) => {
 
     const { id } = req.params
 
-    console.log(id)
-
-    const studentLeaves = await LeaveCollection.find({studentId: id});
-
-    console.log(getReport)
+    const studentLeaves = await LeaveCollection.find({studentId: id}).populate({path: "classId", select: ["-_id", "-createdAt", "-updatedAt", "-__v"]}).select(["-_id", "-__v", "-createdAt", "-updatedAt"])
 
     res.json({ status: "success", studentLeaves: studentLeaves });
 
